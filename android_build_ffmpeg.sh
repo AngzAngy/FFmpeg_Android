7ba93f088d73e50944e7add64ac5e6811a4c3841
ZhangShiyang <angzangy@gmail.com>
2017-01-20 17:28:28 +0800

    ffmpeg-release-2.6 enable h264 acc encoder

---
+++ android_build_ffmpeg.sh
@@ -0,0 +1,45 @@
+export NDK=/home/kuwo/devtools/android-ndk-r13
+SYSROOT=$NDK/platforms/android-9/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --disable-shared \
+    --enable-static \
+    --enable-encoder=aac \
+    --enable-libx264 \
+    --enable-gpl \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --disable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --target-os=linux \
+    --arch=arm \
+    --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+ 
+make clean
+make  -j4 install
+}
+
+#CPU=arm
+#PREFIX=$(pwd)/android/$CPU 
+#OPTIMIZE_CFLAGS="-marm"
+#build_one
+
+#arm v7n
+CPU=armv7-a
+OPTIMIZE_CFLAGS="-O3 -mfloat-abi=softfp -mfpu=neon -marm -march=$CPU -mtune=cortex-a8"
+PREFIX=$(pwd)/android/${CPU}_neon
+ADDITIONAL_CONFIGURE_FLAG=--enable-neon
+build_one
