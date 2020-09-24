73bfa32d5f71323f94809f8aa88f7e145ecbe0ea
AngzAngy <381715271@qq.com>
2020-09-24 05:36:51 -0700

    enable android mediacodec

--- android_build_ffmpeg.sh
+++ android_build_ffmpeg.sh
@@ -1,20 +1,12 @@
-export NDK=/home/kuwo/devtools/android-ndk-r13
+export NDK=/home/angzangy/android-ndk-r13b
 
-CC_VER=4.9
 ARCH=
 EABI=
-TOOLCHAIN=
 SYSROOT=
 ROSSPREFIX=
-TOOLCHAIN_DIR=
 EXTRA_CFLAGS=
 EXTRA_LDFLAGS=
 ADDITIONAL_CONFIGURE_FLAG=
-
-function make_toolchain
-{
-$NDK/build/tools/make-standalone-toolchain.sh --platform=android-21 --toolchain=$TOOLCHAIN$CC_VER --install-dir=$TOOLCHAIN_DIR
-}
 
 function build_one
 {
@@ -23,11 +15,16 @@ function build_one
     --disable-shared \
     --enable-static \
     --enable-libx264 \
-    --enable-libfaac \
+    --enable-libfdk-aac \
     --enable-nonfree \
     --enable-gpl \
     --enable-encoder=libx264 \
-    --enable-encoder=libfaac\
+    --enable-encoder=libfdk-aac \
+    --enable-jni \
+    --enable-mediacodec \
+    --enable-hwaccel=h264_mediacodec \
+    --enable-decoder=h264_mediacodec \
+    --enable-runtime-cpudetect \
     --disable-doc \
     --disable-ffmpeg \
     --disable-ffplay \
@@ -36,13 +33,13 @@ function build_one
     --disable-avdevice \
     --disable-doc \
     --disable-symver \
-    --target-os=linux \
-    --enable-cross-compile \
+    --target-os=android \
     --sysroot=$SYSROOT \
     --arch=$ARCH \
+    --enable-cross-compile \
     --cross-prefix=$ROSSPREFIX \
-    --extra-cflags="-Os -fpic $EXTRA_CFLAGS -I./external/x264libs/$EABI/include -I./external/faaclibs/include" \
-    --extra-ldflags="$EXTRA_LDFLAGS -L./external/x264libs/$EABI/lib -L./external/faaclibs/$EABI" \
+    --extra-cflags="-Os -fPIC $EXTRA_CFLAGS -I`pwd`/external/x264libs/$EABI/include -I`pwd`/external/fdk-aaclibs/$EABI/include" \
+    --extra-ldflags="$EXTRA_LDFLAGS -L./external/x264libs/$EABI/lib -L./external/fdk-aaclibs/$EABI/lib" \
     $ADDITIONAL_CONFIGURE_FLAG 
 
 make clean
@@ -60,50 +57,45 @@ if [ -z "$BUILD_ARCH" ]; then
     exit 1
 fi
 
-TOOLCHAIN_DIR=`pwd`/toolchain/$BUILD_ARCH
-SYSROOT=$TOOLCHAIN_DIR/sysroot/
-
 if [ "$BUILD_ARCH" = "armv7a" ]; then
 	ARCH=arm
 	EABI=armeabi-v7a
-        TOOLCHAIN=arm-linux-androideabi-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/$TOOLCHAIN
-	EXTRA_CFLAGS="-march=armv7-a -mcpu=cortex-a8 -mfpu=vfpv3-d16 -mfloat-abi=softfp -mthumb"
-        EXTRA_LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
-	ADDITIONAL_CONFIGURE_FLAG="--cpu=cortex-a8 --enable-thumb"
+        ROSSPREFIX=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+	SYSROOT=$NDK/platforms/android-21/arch-arm
+	EXTRA_CFLAGS="-mfpu=vfp -mfloat-abi=softfp"
+	ADDITIONAL_CONFIGURE_FLAG="--disable-neon"
 elif [ "$BUILD_ARCH" = "armv5" ]; then
 	ARCH=arm
 	EABI=armeabi
-        TOOLCHAIN=arm-linux-androideabi-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/$TOOLCHAIN
+        ROSSPREFIX=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+	SYSROOT=$NDK/platforms/android-21/arch-arm
 	EXTRA_CFLAGS="-march=armv5te -mtune=arm9tdmi -msoft-float"
 	ADDITIONAL_CONFIGURE_FLAG="--disable-neon"
 elif [ "$BUILD_ARCH" = "armv7aneon" ]; then
 	ARCH=arm
 	EABI=armeabi-v7a
-        TOOLCHAIN=arm-linux-androideabi-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/$TOOLCHAIN
-	EXTRA_CFLAGS="-march=armv7-a -mfpu=neon -mfloat-abi=softfp -mthumb"
-        EXTRA_LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
-	ADDITIONAL_CONFIGURE_FLAG="--cpu=cortex-a8 --enable-thumb --enable-neon"
+        ROSSPREFIX=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+	SYSROOT=$NDK/platforms/android-21/arch-arm
+	EXTRA_CFLAGS="-mfpu=neon -mfloat-abi=softfp"
+	ADDITIONAL_CONFIGURE_FLAG="--enable-neon --disable-asm"
 elif [ "$BUILD_ARCH" = "arm64" ]; then
 	ARCH=arm64
 	EABI=arm64-v8a
-        TOOLCHAIN=aarch64-linux-android-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/$TOOLCHAIN
-	ADDITIONAL_CONFIGURE_FLAG="--enable-thumb"
+        ROSSPREFIX=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+	SYSROOT=$NDK/platforms/android-21/arch-arm64
+	ADDITIONAL_CONFIGURE_FLAG=
 elif [ "$BUILD_ARCH" = "x86" ]; then
 	ARCH=x86
-	EABI=x86	
-        TOOLCHAIN=x86-linux-android-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/i686-linux-android-
+	EABI=x86
+        ROSSPREFIX=$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-
+	SYSROOT=$NDK/platforms/android-21/arch-x86
 	EXTRA_CFLAGS="-march=atom -msse3 -ffast-math -mfpmath=sse"
 	ADDITIONAL_CONFIGURE_FLAG="--cpu=i686 --disable-yasm"
 elif [ "$BUILD_ARCH" = "x86_64" ]; then
 	ARCH=x86_64
-	EABI=x86_64	
-	TOOLCHAIN=x86_64-linux-android-
-        ROSSPREFIX=$TOOLCHAIN_DIR/bin/$TOOLCHAIN
+	EABI=x86_64
+        ROSSPREFIX=$NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/bin/x86_64-linux-android-
+	SYSROOT=$NDK/platforms/android-21/arch-x86_64
 	ADDITIONAL_CONFIGURE_FLAG="--cpu=x86_64 --disable-yasm"
 else
 	echo "unknow architechure $BUILD_ARCH"
@@ -111,5 +103,4 @@ else
 	exit 1
 fi
 
-make_toolchain
 build_one
