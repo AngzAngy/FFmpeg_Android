6a5d63ddf0a1d567951fa90ee22f493d67b9a012
AngzAngy <381715271@qq.com>
2020-09-26 19:15:52 -0700

    fix fdk-aac build script to support neno

--- android_build_fdk-aac.sh
+++ android_build_fdk-aac.sh
@@ -1,20 +1,10 @@
-export NDK=/home/angzangy/android-ndk-r12b
-CC_VER=4.9
-
-function make_toolchain
-{
-$NDK/build/tools/make-standalone-toolchain.sh --platform=android-21 --toolchain=$TOOLCHAIN$CC_VER --install-dir=$TOOLCHAIN_DIR
-}
+export NDK=/home/angzangy/android-ndk-r13b
 
 function build_aac
 {
 ./autogen.sh
 ./configure --prefix=$PREFIX \
-            --host=$HOST \
-	    LDFLAGS=$LDFLAGS \
-            CFLAGS=$CFLAGS \
-            CPPFLAGS=$CFLAGS \
-            CXXFLAGS=$CFLAGS
+            --host=$HOST
 
 make clean
 make -j4
@@ -29,51 +19,80 @@ if [ -z "$BUILD_ARCH" ]; then
     exit 1
 fi
 
-TOOLCHAIN_DIR=`pwd`/toolchain/$BUILD_ARCH
-SYSROOT=$TOOLCHAIN_DIR/sysroot
-###  add PATH env is important  ###
-export PATH=$PATH:$TOOLCHAIN_DIR/bin
-
 if [ "$BUILD_ARCH" = "armv7a" ]; then
-	TOOLCHAIN=arm-linux-androideabi-
+        TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-arm
+
+	export CC="$TOOLCHAIN/bin/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/arm-linux-androideabi-g++ --sysroot=$SYSROOT"
+	export CFLAGS="-mfpu=vfp -mfloat-abi=softfp"
+	export CXXFLAGS="$CFLAGS"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=arm-linux-androideabi
 	PREFIX=`pwd`/../fdk-aaclibs/armeabi-v7a
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
+
+	echo "start---------------------------->"
+	echo $PATH
 elif [ "$BUILD_ARCH" = "armv5" ]; then
-	TOOLCHAIN=arm-linux-androideabi-
+        TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-arm
+
+	export CC="$TOOLCHAIN/bin/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/arm-linux-androideabi-g++ --sysroot=$SYSROOT"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=arm-linux-androideabi
 	PREFIX=`pwd`/../fdk-aaclibs/armeabi
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
 elif [ "$BUILD_ARCH" = "armv7aneon" ]; then
-	TOOLCHAIN=arm-linux-androideabi-
+        TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-arm
+
+	export CC="$TOOLCHAIN/bin/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/arm-linux-androideabi-g++ --sysroot=$SYSROOT"
+	export CFLAGS="-mfpu=neon -mfloat-abi=softfp"
+	export CXXFLAGS="$CFLAGS"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=arm-linux-androideabi
 	PREFIX=`pwd`/../fdk-aaclibs/armeabi-v7a-neon
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
 elif [ "$BUILD_ARCH" = "arm64" ]; then
-        TOOLCHAIN=aarch64-linux-android-
+	TOOLCHAIN=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-arm64
+
+	export CC="$TOOLCHAIN/bin/aarch64-linux-android-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/aarch64-linux-android-g++ --sysroot=$SYSROOT"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=aarch64-linux-android
 	PREFIX=`pwd`/../fdk-aaclibs/arm64-v8a
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
-elif [ "$BUILD_ARCH" = "x86" ]; then	
-        TOOLCHAIN=x86-
+elif [ "$BUILD_ARCH" = "x86" ]; then
+	TOOLCHAIN=$NDK/toolchains/x86-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-x86
+
+	export CC="$TOOLCHAIN/bin/i686-linux-android-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/i686-linux-android-g++ --sysroot=$SYSROOT"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=i686-linux-android
 	PREFIX=`pwd`/../fdk-aaclibs/x86
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
 elif [ "$BUILD_ARCH" = "x86_64" ]; then
-	TOOLCHAIN=x86_64-
+	TOOLCHAIN=$NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64
+        SYSROOT=$NDK/platforms/android-21/arch-x86_64
+
+	export CC="$TOOLCHAIN/bin/x86_64-linux-android-gcc --sysroot=$SYSROOT"
+	export CXX="$TOOLCHAIN/bin/x86_64-linux-android-g++ --sysroot=$SYSROOT"
+	export LDFLAGS="-L$SYSROOT/usr/lib"
+
 	HOST=x86_64-linux-android
 	PREFIX=`pwd`/../fdk-aaclibs/x86_64
-	CFLAGS="-I$SYSROOT/usr/include"
-	LDFLAGS="-L$SYSROOT/usr/lib"
 else
 	echo "unknow architechure $BUILD_ARCH"
         echo "You must specific an architecture 'armv5, armv7a, armv7aneon, arm64, x86, x86_64...'."
 	exit 1
 fi
-make_toolchain
+
+###  add PATH env is important  ###
+export PATH=$PATH:$TOOLCHAIN/bin
+
 build_aac
